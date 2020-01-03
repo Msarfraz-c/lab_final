a5e461540819e5a46a1032d10f4f657e00566cda
muham <muham@DESKTOP-6BRGSHP>
2020-01-03 16:26:47 +0500

    first

---
+++ calcu/src/test/java/calcu/CalculatorTest.java
@@ -0,0 +1,41 @@
+package calcu;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.AfterClass;
+
+public class CalculatorTest {
+private static Calculator calculator;
+@BeforeClass
+public static void setUpBeforeClass() throws Exception {
+System.out.println("before class");
+calculator = new Calculator();
+		}
+@Test  
+public void testFindMax(){  
+assertEquals(4,calculator.findMax(1,3,4));  
+//assertEquals(-1,calculator.findMax(-12,-1,-2));  
+		} 
+@Test  
+		public void testSquare1(){  
+			assertEquals(9,calculator.square(3));  
+		}
+		@Test (expected = IllegalArgumentException.class) 
+		public void testSquare2(){  
+			calculator.square(-1);  
+		}
+		@Test  
+		public void testCube(){  
+			assertEquals(27,calculator.cube(3));  
+		}
+		@AfterClass  
+	    public static void tearDownAfterClass() throws Exception {  
+	        System.out.println("after class");  
+	    }  
+	}
+
+
